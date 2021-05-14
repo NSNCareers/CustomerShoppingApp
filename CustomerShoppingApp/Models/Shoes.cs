7f17ce88ba6ef8e1e9ee9be4b144dbdb11197d47
Ajang elvis Ngwesse <NSNCareers@outlook.com>
2021-05-14 17:26:30 +0100

    models

---
+++ CustomerShoppingApp/Models/Shoes.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerShoppingApp.Models
+{
+    public class Shoes
+    {
+        public int id { get; set; }
+        public double price { get; set; }
+        public float size { get; set; }
+        public string brand { get; set; }
+        public string colour { get; set; }
+    }
+}
